dd83a48629930b352b1fbe42d35fd4dd137034b4
pawan67 <pawantamada8@gmail.com>
2022-01-12 10:10:20 +0530

    ERRROEERROE

--- pages/firstyear.js
+++ pages/firstyear.js
@@ -1,8 +1,19 @@
 import Link from "next/link";
 
 import Header from "../components/Header";
+import Subject from "../components/Subject";
 import Head from "next/head";
+import { useState } from "react";
 function Firstyear() {
+  const [sem, setSem] = useState();
+  if (sem == "1") {
+    return <div className="background">
+      <div className="innerdiv">
+        <Header/>
+        <Subject/>
+      </div>
+    </div>;
+  }
   return (
     <>
       <Head>
@@ -11,7 +22,20 @@ function Firstyear() {
           url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700&display=swap');
         </style>
       </Head>
-      
+      <div className="background">
+        <div className=" innerdiv">
+          <Header />
+          <div className=" w-80 mx-auto mt-16">
+            <img src="svg/teaching.svg" alt="" />
+          </div>
+          <div className=" mt-16 space-y-5">
+            <button onClick={() => setSem("1")} className=" button">
+              1st Semester
+            </button>
+            <button className=" button">2nd Semester</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

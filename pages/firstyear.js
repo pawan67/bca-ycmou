79d43584ade06eed286e396297e4d1f421afe6e6
pawan67 <pawantamada8@gmail.com>
2022-01-12 09:47:40 +0530

    chup gaandu

--- pages/firstyear.js
+++ pages/firstyear.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import FirstPage from "../components/FirstPage";
+
 import Header from "../components/Header";
 import Head from "next/head";
 function Firstyear() {
@@ -11,12 +11,7 @@ function Firstyear() {
           url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700&display=swap');
         </style>
       </Head>
-      <FirstPage
-        firstLink="/firstyearDashboardFirstSem"
-        secondLink="/firstyearDashboardSecondSem"
-        firstSem="1st Semester"
-        secondSem="2nd Semester"
-      />
+      
     </>
   );
 }
